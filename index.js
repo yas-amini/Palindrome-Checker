f07c1a8cb67c06f785bc074cc9ea34ae215c7afa
Yasamin <yasamin.amini98@gmail.com>
2024-08-08 12:24:28 +0200

    Commenting on JavaScript

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// Select the input element by its ID "input" and store it in the variable 'input'
 const input = document.getElementById("input")
-
+// Function to reverse a given string
 function reverseString(str){
+    //splits the string into an array of characters, reverses the array, then joins it back into a string 
     return str.split("").reverse().join("")
 }
-//Getting the value of the text input
+//Checking if the input value is a palindrome
 function check() {
+    // Gets the current value from the input field
     const value = input.value;
+    // Reverses the input value using the reverseString function
     const reverse = reverseString(value)
-
+    // Compares the original vaLue with the reversed value
     if (value === reverse){
+        // If they are equal, displays an alert with the text PALINDROME
         alert ( "P A L I N D R O M E")
     } else {
+        // If not equal, displays "Not today!"
         alert("Not today!")
     }
 }
